574b23ad96dbcac5d193ac93c259b6b9fb281aa5
KIIT <ps749924@gmail.com>
2021-03-04 09:43:47 +0530

    hours and minutes

--- LabPractice/q4.py
+++ LabPractice/q4.py
@@ -1,1 +1,11 @@
-a = 10
+'''
+Given the integer N - the number of minutes that is passed since midnight - how many hours and minutes are
+displayed on the 24h gigital clock?
+The program should print two number: the number of hours (between 0 and 23) and the number of minutes
+between 0 and 59).
+'''
+
+N=int(input("Enter the minutes passed since midnight: "))
+hours=N//60
+minutes=N%60
+print(f"{hours}hours and {minutes}has passed")
