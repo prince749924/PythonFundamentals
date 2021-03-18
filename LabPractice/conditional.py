272080273c6ae3b8dbd9629b69180fc8e49d377b
KIIT <ps749924@gmail.com>
2021-03-18 10:00:53 +0530

    area of circle

---
+++ LabPractice/conditional.py
@@ -0,0 +1,16 @@
+num=int(input('Enter the number:'))
+if num==1:
+    print('The day is sunday')
+elif num==2:
+    print('The day is monday')
+elif num==3:
+    print('The day is tuesday')
+elif num==4:
+    print('The day is wednesday')
+elif num==5
+    print(T)
+
+
+
+
+
---
+++ LabPractice/userage.py
@@ -0,0 +1,3 @@
+#write a program to take a input from the users age.
+#if the users age is below 15 print a message(u r a child).
+#if the users age is greater than 15 and lesser than 40 print a message(u are adult),if user is greater than 40 print a message u r old
